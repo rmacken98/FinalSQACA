9b775af64d9dafcfde72bacb5094651a1a15c48a
Ronan Macken <ronan.macken@mydit.ie>
2021-05-19 23:10:01 +0100

    createRubric method implemented

--- src/Controller.java
+++ src/Controller.java
@@ -1,3 +1,12 @@
+import java.util.ArrayList;
 public class Controller {
-    
+  
+    // creates a new Rubric.
+    public static Rubric createRubric(String name, ArrayList<Criterion> criteria) {
+		
+		Rubric newRubric = new Rubric(name, criteria); 
+				return newRubric;
+		
+	}
+	
 }
--- src/Tests.java
+++ src/Tests.java
@@ -1,3 +1,22 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
 public class Tests {
     
+  // Test creation of Rubric object
+  @Test
+  public void createRubricWithCriterion() {
+        Criterion c1 = new Criterion("Documentation");
+        ArrayList<Criterion> criteria = new ArrayList<Criterion>();
+        criteria.add(c1);
+        Rubric rubric=  Controller.createRubric("Final Year Project", criteria);
+        assertTrue("Rubric created with criterion added",rubric instanceof Rubric);
+  }
+
+
+
+
 }
