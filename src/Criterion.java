bfe2b01970dc40a9e057946598c35eb876c33ce1
Ronan Macken <ronan.macken@mydit.ie>
2021-05-19 22:02:11 +0100

    Created criterion

--- src/Criterion.java
+++ src/Criterion.java
@@ -1,3 +1,31 @@
 public class Criterion {
     
-}
+    private String criteria;
+    private int score;
+    
+    
+    public Criterion(String criteria) {
+        
+        criteria = this.criteria;
+    }
+    public String getCriteria() {
+        return criteria;
+    }
+    
+    
+    public void setCriteria(String criteria) {
+        this.criteria = criteria;
+    }
+    
+    
+    public int getScore() {
+        return score;
+    }
+    
+    
+    public void setScore(int score) {
+        this.score = score;
+    }
+    
+    }
+    
